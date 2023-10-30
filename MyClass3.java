0e3e83a471a21424687ca2651cb3a658c1321b81
ASHISH PATEL <ASHISHPATELUK@GMAIL.COM>
2023-10-30 16:58:19 +0000

    P3 Commit

---
+++ MyClass3.java
@@ -0,0 +1,14 @@
+package mypackage3;
+
+
+
+public class MyClass3
+{
+
+    public static void main(String[]args)
+
+    {
+        System.out.println("What are Challenges Of The Tester?");
+    }
+
+}
